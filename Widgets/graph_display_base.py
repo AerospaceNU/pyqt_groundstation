0dc7878c11e2ee4616827fed44c5920b64e7b8b8
shoti411 <po.luk@northeastern.edu>
2022-02-21 20:53:07 -0500

    Graph now shows

--- Widgets/graph_display_base.py
+++ Widgets/graph_display_base.py
@@ -1,7 +1,7 @@
 
 import PyQt5.QtCore as QtCore
 import time
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QGridLayout
 from Widgets import custom_q_widget_base
 import sys
 from PyQt5.QtChart import QChart, QChartView, QLineSeries
@@ -38,6 +38,8 @@ class GraphDisplay(custom_q_widget_base.CustomQWidgetBase):
 
         series.append(xTimePos, yAltitudePos)
 
+        #self.create_linechart()
+
         chart = QChart()
         chart.addSeries(series)
         chart.createDefaultAxes()
@@ -50,36 +52,12 @@ class GraphDisplay(custom_q_widget_base.CustomQWidgetBase):
 
         chartview = QChartView(chart)
         chartview.setRenderHint(QPainter.Antialiasing)
+
+        layout = QGridLayout()
+        layout.addWidget(chartview)
+        self.setLayout(layout)
 
         self.show()
-
-
-# Data for Linechart
-    def create_linechart(self):
-        series = QLineSeries(self)
-        series.append(0, 6)
-        series.append(2, 4)
-        series.append(3, 8)
-        series.append(7, 4)
-        series.append(10, 5)
-
-        series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2)
-
-        chart = QChart()
-
-        chart.addSeries(series)
-        chart.createDefaultAxes()
-        chart.setAnimationOptions(QChart.SeriesAnimations)
-        chart.setTitle("Line Chart Example")
-
-        # Legend of Chart
-        chart.legend().setVisible(True)
-        chart.legend().setAlignment(Qt.AlignBottom)
-
-        chartview = QChartView(chart)
-        chartview.setRenderHint(QPainter.Antialiasing)
-
-        #self.setCentralWidget(chartview)
 
 
     def addPoint(self, point):
