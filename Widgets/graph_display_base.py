132961da94dbcd95a026077130dfe933f306c1e7
Nathan McGuire <nathan.e.mcguire@gmail.com>
2022-03-03 11:23:41 -0500

    Slightly nicer altitude graph

--- Widgets/graph_display_base.py
+++ Widgets/graph_display_base.py
@@ -6,9 +6,6 @@ os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")  # https://stackoverflow.com/quest
 
 from PyQt5.QtWidgets import QWidget, QGridLayout
 from pyqtgraph import PlotWidget
-from PyQt5.QtCore import QPointF
-from PyQt5.QtGui import QPainter
-from PyQt5.QtCore import Qt
 
 from constants import Constants
 from Widgets.custom_q_widget_base import CustomQWidgetBase
@@ -16,17 +13,18 @@ from data_helpers import get_value_from_dictionary, interpolate
 
 
 class GraphDisplay(CustomQWidgetBase):
-    def __init__(self, parentWidget: QWidget = None, pointsToKeep=200, updateInterval=3):
+    def __init__(self, parentWidget: QWidget = None, pointsToKeep=200):
         super().__init__(parentWidget)
 
-        self.updateInterval = updateInterval
-        self.setWindowTitle("PyQtChart Line")
         self.setGeometry(100, 100, 680, 500)
 
         self.graphWidget = PlotWidget()
+        self.graphWidget.setTitle("Altitude")
+        self.graphWidget.setLabel('bottom', "Time (s)")
 
         self.time_list = []
         self.value_list = []
+        self.start_time = time.time()
 
         self.data_line = self.graphWidget.plot(self.time_list, self.value_list)
 
@@ -39,7 +37,7 @@ class GraphDisplay(CustomQWidgetBase):
     def updateData(self, vehicleData):
         altitude = float(get_value_from_dictionary(vehicleData, Constants.altitude_key, 0))
 
-        self.time_list.append(time.time())
+        self.time_list.append(time.time() - self.start_time)
         self.value_list.append(altitude)
 
         self.data_line.setData(self.time_list, self.value_list)
