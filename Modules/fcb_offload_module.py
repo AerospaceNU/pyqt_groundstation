e9a05f93c51af35ff43ab32aee433662936d16d8
Matthew Morley <matthew.morley.ca@gmail.com>
2022-05-19 16:29:22 -0700

    Update fcb_offload_module.py

--- Modules/fcb_offload_module.py
+++ Modules/fcb_offload_module.py
@@ -64,7 +64,7 @@ class FCBOffloadModule(ThreadedModuleCore):
                 if "Available flights to offload" in ret:  # Check and see if we have a list of flights, and update the database dictionary
                     self.data_dictionary[Constants.cli_flights_list_key] = ret
 
-            self.cliConsole.autoAddEntry(ret, True)
+                self.cliConsole.autoAddEntry(ret, True)
 
         time.sleep(0.1)
 
