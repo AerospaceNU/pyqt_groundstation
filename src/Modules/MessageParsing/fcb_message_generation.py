904ed2458044a3da698ca68e4df1fba998492c48
AeronuAvionics <avionicslaptop@gmail.com>
2025-04-30 20:33:46 -0400

    FCB packet type fix

--- src/Modules/MessageParsing/fcb_message_generation.py
+++ src/Modules/MessageParsing/fcb_message_generation.py
@@ -8,13 +8,13 @@ packet_num = 0
 
 
 def createRadioBandCommandMessage(destination, target_radio, channel):
-    return struct.pack("<BHBBb", destination, 3, 1, target_radio, channel)  # destination, length, command id, target radio, channel
+    return struct.pack("<BBHBBb", destination, 0, 3, 1, target_radio, channel)  # destination, length, command id, target radio, channel
 
 
 def createCLICommandMessage(destination, command: str):
     command = command.strip() + "\n"
     command_bytes = command.encode()
-    return struct.pack("<BH", destination, len(command_bytes)) + command_bytes
+    return struct.pack("<BBH", destination, 0, len(command_bytes)) + command_bytes
 
 
 def createRadioPacket(packet_type, payload: bytes):
